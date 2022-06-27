e1387a249e21db2d849141dea4e7e267d0f10c2a
Sophoca <tjdqls1668@naver.com>
2022-06-27 15:52:14 +0900

    init commit

---
+++ src/pages/test.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+// markup
+const TestPage = () => {
+  return (
+    <main>
+      <title>TestPage</title>
+      <h1>TestPage</h1>
+    </main>
+  );
+};
+
+export default TestPage;

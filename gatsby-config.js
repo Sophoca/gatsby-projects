6a439d5ba949962ca51c6a56c1059b6b7c947a0f
Sophoca <tjdqls1668@naver.com>
2022-06-28 16:57:03 +0900

    chore: add mdx plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,17 @@ module.exports = {
     title: "sbhong.log",
     description: "안녕하세요. 백엔드 개발자를 꿈꾸는 홍성빈입니다.",
   },
-  plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
+  plugins: [
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      },
+    },
+    "gatsby-plugin-mdx",
+    "gatsby-transformer-sharp",
+  ],
 };

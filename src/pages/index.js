9c2698dce623de92c19beca50e8ac62fdb2fa593
Sophoca <tjdqls1668@naver.com>
2022-06-28 14:21:03 +0900

    feat: add layout components and css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
 import * as React from "react";
+import Layout from "../components/layout";
 
 // markup
 const IndexPage = () => {
   return (
-    <main>
-      <title>HomePage</title>
-      <h1>HomePage</h1>
-    </main>
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the Gatsby Tutorial.</p>
+    </Layout>
   );
 };
 

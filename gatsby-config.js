a45076913b7661d05ef6954b3adcc4d98ef76086
Sophoca <tjdqls1668@naver.com>
2022-06-28 16:48:11 +0900

    feat: add image component

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: "My First Gatsby Site",
+    title: "sbhong.log",
+    description: "안녕하세요. 백엔드 개발자를 꿈꾸는 홍성빈입니다.",
   },
   plugins: ["gatsby-plugin-image", "gatsby-plugin-sharp"],
 };
